07f84a37acf811526b10b8d52f4a03131da8a4a1
haungxi <1842196429@qq.com>
2024-07-17 10:47:03 +0800

    ✨ feat(压缩gzip): 压缩gzip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import viteCompression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "gzip",
+      threshold: 10240,
+      verbose: true, // 是否在控制台中输出压缩结果
+      ext: ".gz",
+      deleteOriginFile: true, // 源文件压缩后是否删除
+    }),
+  ],
 })
